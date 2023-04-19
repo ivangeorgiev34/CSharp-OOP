2dc4860df7236e4e712394ce9a5e467118908189
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-19 20:04:28 +0300

    Add exercise 05.StackOfStrings

---
+++ Inheritance - Lab/05.StackOfStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //StackOfStrings stack = new StackOfStrings();
+
+
+            //Console.WriteLine(stack.IsEmpty());
+            //stack.AddRange(new List<string>() { "1", "2", "3", "4", "5" });
+            //Console.WriteLine(stack.IsEmpty());
+            //while (stack.Count>0)
+            //{
+            //    Console.WriteLine(stack.Pop());
+            //}
+        }
+    }
+}
---
+++ Inheritance - Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count == 0;
+        } 
+        public Stack<string> AddRange( List<string> items)
+        {
+            foreach (var item in items)
+            {
+                Push(item);
+            }
+            return this;
+        } 
+    }
+}
