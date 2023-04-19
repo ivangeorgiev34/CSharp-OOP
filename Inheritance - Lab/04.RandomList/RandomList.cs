392f15ce6779757cf7fffd8a6392fed4da218e52
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-19 20:04:11 +0300

    Add exercise 04.RandomList

---
+++ Inheritance - Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+   public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random r = new Random();
+            int index = r.Next(0, Count);
+            string element = this[index];
+            RemoveAt(index);
+            return element;
+        }
+    }
+}
