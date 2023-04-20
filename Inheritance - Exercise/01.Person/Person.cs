83849a0048727e94e8aa2524f5075962ceb0f003
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-20 21:30:17 +0300

    Add exercise 01.Person

---
+++ Inheritance - Exercise/01.Person/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+   public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
