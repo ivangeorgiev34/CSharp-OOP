dd0b9ea2bd02c0d6c6d79eef5fe69d109f6009cf
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-20 21:36:54 +0300

    Add exercise 04.NeedForSpeed

---
+++ Inheritance - Exercise/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DEFAULT_VEHICLE_FUEL_CONSUMPTION = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+        public virtual double FuelConsumption { get { return DEFAULT_VEHICLE_FUEL_CONSUMPTION; }  }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+        public  void Drive(double kilometers)
+        {
+            if (Fuel - (kilometers * FuelConsumption) >= 0)
+            {
+                Fuel = Fuel - (kilometers * FuelConsumption);
+            }
+        }
+    }
+}
