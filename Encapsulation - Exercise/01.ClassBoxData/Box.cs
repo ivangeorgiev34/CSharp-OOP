6e98ba386aaa32d8c1de9c9e559a7230afc01d5c
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-24 21:25:59 +0300

    Add exercise 01.ClassBoxData

---
+++ Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box
+    {
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        private double length;
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        private double width;
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        private double height;
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+        public double SurfaceArea()
+        {
+            return 2 * (Length * Width + Length * Height + Width * Height);
+        }
+        public double LateralSurfaceArea()
+        {
+            return 2 * (Length * Height + Width * Height);
+        }
+        public double Volume()
+        {
+            return Width * Length * Height;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {SurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {Volume():f2}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
