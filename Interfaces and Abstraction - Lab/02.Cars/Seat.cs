3347c751120f2e9ca4d0f6717205eddfe4ad5f53
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-04-25 19:08:58 +0300

    Add exercise 02.Cars

---
+++ Interfaces and Abstraction - Lab/02.Cars/Seat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get;  set; }
+        public string Color { get;  set; }
+
+        public string Start()
+        {
+            return "";
+        }
+
+        public string Stop()
+        {
+            return "";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}";
+        }
+    }
+}
---
+++ Interfaces and Abstraction - Lab/02.Cars/Tesla.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "";
+        }
+
+        public string Stop()
+        {
+            return "";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries";
+        }
+    }
+}
