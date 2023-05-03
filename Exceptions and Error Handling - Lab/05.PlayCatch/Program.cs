36e4e1fac7575189fe6fdd7c53614deaecb1d4e0
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-05-03 20:38:20 +0300

    Add exercise 05.PlayCatch

---
+++ Exceptions and Error Handling - Lab/05.PlayCatch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PlayCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            int exceptionsCount = 0;
+
+            while (exceptionsCount < 3)
+            {
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    if (command[0] == "Replace")
+                    {
+                        int index = int.Parse(command[1]);
+                        int element = int.Parse(command[2]);
+                        input[index] = element;
+                    }
+                    else if (command[0] == "Print")
+                    {
+                        int startIndex = int.Parse(command[1]);
+                        int endIndex = int.Parse(command[2]);
+                        List<int> list = new List<int>();
+                        for (int i = startIndex; i <= endIndex; i++)
+                        {
+                            list.Add(input[i]);
+                        }
+                        Console.WriteLine(string.Join(", ",list));
+                    }
+                    else if (command[0] == "Show")
+                    {
+                        int index = int.Parse(command[1]);
+                        Console.WriteLine(input[index]);
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The index does not exist!");
+                    exceptionsCount++;
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                    exceptionsCount++;
+                }
+            }
+            Console.WriteLine(string.Join(", ",input));
+        }
+    }
+}
