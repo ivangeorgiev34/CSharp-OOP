2054395adcfcada4453ebc90a14ecac098a8110f
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-05-03 20:36:17 +0300

    Add exercise 01.SquareRoot

---
+++ Exceptions and Error Handling - Lab/01.SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                double result = Math.Sqrt(number);
+                if (double.IsNaN(result))
+                {
+                    throw new ArithmeticException("Invalid number.");
+                }
+                Console.WriteLine(result);
+            }
+            catch (ArithmeticException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
