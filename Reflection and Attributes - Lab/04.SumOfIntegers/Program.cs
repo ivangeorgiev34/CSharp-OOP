19e7c0084e700d8dbb76e2ff00acc6b83cc5a501
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-05-04 22:09:05 +0300

    Add exercise 04.SumOfIntegers

---
+++ Reflection and Attributes - Lab/04.SumOfIntegers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SumOfIntegers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().ToArray();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                try
+                {
+                    int number = int.Parse(input[i]);
+                    stack.Push(number);
+
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{input[i]}' is out of range!");
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine($"The element '{input[i]}' is in wrong format!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{input[i]}' processed - current sum: {stack.Sum()}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {stack.Sum()}");
+        }
+    }
+}
