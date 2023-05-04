4a6edf995e63093ee67eeab74121d96bfffd98bd
Ivan Georgiev <114490562+ivangeorgiev34@users.noreply.github.com>
2023-05-04 22:07:03 +0300

    Add exercise 01.Stealer

---
+++ Reflection and Attributes - Lab/01.Stealer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            Console.WriteLine(spy.StealFieldInfo("Stealer.Hacker", new string[]{ "username","password"}));
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, string[] fieldsToInvestigate)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type type = Type.GetType(className);
+            sb.AppendLine($"Class under investigation: {type.FullName}");
+            Object instance = Activator.CreateInstance(type, new string[] { });
+            FieldInfo[] requestedFields = type.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (FieldInfo field in requestedFields.Where(f => fieldsToInvestigate.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+        
+
+        
+    }
+}
